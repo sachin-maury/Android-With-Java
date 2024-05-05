1550e6ba80142dde07961f24ecd0b6b2f011c942
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-05 20:49:44 +0530

    while loop

---
+++ day1/practice2.java
@@ -0,0 +1,13 @@
+
+
+public class practice2 {
+public static void main(String[] args) {
+    int a =0;
+    String name ="Hello how are you ";
+    while(a<8){
+        System.out.println(name);
+        a++;
+    }
+}
+    
+}
