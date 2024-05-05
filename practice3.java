8574cd8cfcc2af9a047a1ca8b251b6dbaaf7d1c6
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-05 20:55:57 +0530

    do while loop

---
+++ practice3.java
@@ -0,0 +1,13 @@
+public class practice3 {
+    public static void main(String[] args) {
+        int a=0;
+        String name = "monu is a good boy in your class :";
+        do{
+            System.out.println(name);
+            a++;
+        }
+        while(a>5); // one time running because condition is false
+            
+        
+    }
+}
