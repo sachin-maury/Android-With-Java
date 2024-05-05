095f3d1e8f9ddf0ce080584b69c7e29617b162f5
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-05 21:07:50 +0530

    String program toLowerCase() toUpperCase() concat()

---
+++ practice4.java
@@ -0,0 +1,8 @@
+public class practice4 {
+    public static void main(String[] args) {
+        String name = "Human";
+        System.out.println("The length of String : "+ name.toLowerCase());
+        System.out.println("The length of String : "+ name.toUpperCase());
+        System.out.println("The length of String : "+ name.concat("from Delhi"));
+    }
+}
