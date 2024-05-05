97eaf671be29b3b2c14f48539034ff41d1f4b7a7
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-05 20:30:21 +0530

    if else program

---
+++ day1/practice1.java
@@ -0,0 +1,19 @@
+
+
+public class practice1 {
+    public static void main(String[] args) {
+      //  int num =100;
+      //  if(num>200){
+      //      System.out.println("Number accepted !");
+      //  }
+        int marksHindi = 96;
+        if(marksHindi>95){
+            System.out.println("We will be gifted bike");
+        }
+        else{
+            System.out.println("Not gigted anythgin for you ");
+        }
+        
+    }
+    
+}
