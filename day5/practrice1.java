9526002ddd7a7c9398ec8e91e3c686da4c17f46a
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-09 11:52:38 +0530

    practice

---
+++ day5/practrice1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class practrice1 {
+
+    public static int printNumber(int a,int b){
+        int sum =a+b;
+       
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Enter your number : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = printNumber(a, b);
+        System.out.println("The sum is : "+sum);
+
+
+     }
+}
