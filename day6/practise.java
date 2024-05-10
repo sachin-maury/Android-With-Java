a2f25b68584249c03aeb625ca78506df6c33c06b
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-10 10:27:27 +0530

    practice

---
+++ day6/practise.java
@@ -0,0 +1,24 @@
+class Animal{
+  public void eat(){
+    System.out.println("Animal is eating :");
+  }
+  class Dog extends Animal{
+    public void bark(){
+        System.out.println("Dog is a barking :");
+    }
+    public void sound(){ 
+        System.out.println("Dog is make sound :");
+    }
+  }
+}
+
+public class practise {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+       Dog dog = new Dog();
+       dog.bark();
+       dog.sound();
+
+    }
+}
