11589a0a20ad0e2c16055bc2b6266b0622f2863c
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-22 22:51:32 +0530

    swing java

---
+++ day14/SwingExample.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class SwingExample {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("My Swing Application");
+    JButton button = new JButton("Click Me");
+    JLabel label = new JLabel("Click the button!");
+    frame.add(button); 
+    frame.add(label);
+    frame.setSize(400, 200);
+    frame.setVisible(true); 
+  }
+}
+
