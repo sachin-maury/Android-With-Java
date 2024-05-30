aaaba26c986157df7a2f240d7cbed06ffe79e304
Sachin Maurya <kumarsachin67671@gmail.com>
2024-05-30 11:02:52 +0530

    Find Key element in Array

---
+++ day19/array.java
@@ -0,0 +1,26 @@
+
+
+class array {
+    public static int binarySearch(int numbers [] , int key){
+        int start =0, end = numbers.length;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(numbers [mid]== key){
+                    return mid;
+            }
+            if(numbers[ mid]<key){
+                start = mid+1; 
+            }
+            else{
+                end= mid-1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers [] ={2,4,6,8,10,12,14};
+        int key =10;
+        System.out.println("The binary search key Value : "+binarySearch(numbers, key));
+    }
+}
