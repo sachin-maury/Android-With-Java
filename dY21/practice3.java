414d5afa3845d9c19c611297aa84843a36f7cd8e
Sachin Maurya <kumarsachin67671@gmail.com>
2024-12-29 20:40:45 +0530

    p

---
+++ dY21/practice3.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+public class practice3 {
+    // Source code is decompiled from a .class file using FernFlower decompiler.
+
+
+
+   public p() {
+   }
+
+   public static int printNumber(int var0, int var1) {
+      int var2 = var0 + var1;
+      return var2;
+   }
+///
+   public static void main(String[] var0) {
+      System.out.println("Enter your number : ");
+      Scanner var1 = new Scanner(System.in);
+      int var2 = var1.nextInt();
+      int var3 = var1.nextInt();
+      int var4 = printNumber(var2, var3);
+      System.out.println("The sum is : " + var4);
+   }
+}
+
+}
