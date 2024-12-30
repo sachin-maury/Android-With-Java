05c7f697536a96e11a841b6a37a157b80b4e47e7
Sachin Maurya <kumarsachin67671@gmail.com>
2024-12-30 21:00:10 +0530

    practice

---
+++ day2/even.java
@@ -0,0 +1,23 @@
+public class even {
+    
+
+
+
+    public static void main(String[] args) {
+        
+    
+    int a[] ={45,84,65,85,28,42};
+    for(int i=0; i<a.length; i++){
+    if(a[i]%2==0){
+        System.out.println("This is a even number : "+a[i]);
+    }
+    else{
+        System.out.println("This is a odd number : "+a[i]);
+    }
+}
+
+}
+    
+}
+
+
